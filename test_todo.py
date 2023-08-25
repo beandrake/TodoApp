49913502db2c269f90acbfbeb11e372a3103e66f
Branden Bean Drake <30214543+beandrake@users.noreply.github.com>
2023-08-25 15:54:30 -0700

    Removed some unintended reference code.  Added more comprehensive tests for adding and getting.  Changed design to have the database live in memory with explicit save and load sequences, rather than constantly writing and reading from a file with every SQL command.  At present all tests pass, with the exception of the new test I hastily wrote to verify data types when I learned that sqlite3 doesn't enforce data types itself.  That will be for next time.

--- test_todo.py
+++ test_todo.py
@@ -1,29 +1,69 @@
 import pytest
 from pytest import raises
 from pytest import approx
-from sqlite3 import ProgrammingError
+from sqlite3 import ProgrammingError, IntegrityError
 from todo import Todo
 
 # Note: a new fixture is generated for every test
 @pytest.fixture(autouse=False)
 def myTodo():
-	thing = Todo(":memory:")
+	thing = Todo()
 	return thing
 
+def test_getNotes_empty(myTodo):
+	noteList = myTodo.getNotes()
+	assert noteList == []
 
-def test_addNote(myTodo):
+def test_addNote_single(myTodo):
 	text = r"Clean your room"
 	priority = 3.3
-	dueDate = "2024-03-01"
-	myTodo.addNote(text, priority, dueDate)
+	completedOn = "2024-03-01"
+	myTodo.addNote(text, priority, completedOn)
 	noteList = myTodo.getNotes()
 	note = noteList[0]
-	assert note[1] == text and note[2] == approx(3.3) and note[3] == dueDate
+	assert note[0] == text and note[1] == approx(priority) and note[2] == completedOn
 
+def test_addNote_multiple(myTodo):	
+	# Note: when retrieved, lower priority will be earlier in list
+	testData0 = ["Get groceries for the week", 12.1, "2023-02-14"]
+	testData1 = ["Clean my room", 20, None]
+	testData2 = ["135091241351352460989", 1000]
+	
+	# inserting in non-linear order to test ordering by priority
+	myTodo.addNote(*testData1)
+	myTodo.addNote(*testData2)
+	myTodo.addNote(*testData0)
+
+	# Returned data will contail null values for dates that weren't provided, so let's add that to our test data
+	testData2.append(None)
+
+	# Now convert testData to tuples
+	testList = [ tuple(testData0), tuple(testData1), tuple(testData2)]	
+
+	noteList = myTodo.getNotes()
+	assert testList == noteList
+
+
+def test_addNote_duplicate(myTodo):
+	myTodo.addNote(r'SameExactText', 1)
+	with raises(IntegrityError):
+		myTodo.addNote(r'SameExactText', 2)
+
+def test_addNote_below_1(myTodo):
+	with raises(ValueError):
+		myTodo.addNote(r'Should fail because value is negative', 0.999)
 
 def test_addNote_negative(myTodo):
 	with raises(ValueError):
 		myTodo.addNote(r'Should fail because value is negative', -5)
+
+def test_addNote_wrongDataTypes(myTodo):
+	with raises(ValueError):
+		myTodo.addNote(r'abc', 'xyz')
+	with raises(ValueError):
+		myTodo.addNote(123, 123)
+	with raises(ValueError):
+		myTodo.addNote(r'Task', 21, 'Steven')
 
 
 def test_clearNotes(myTodo):
--- todo.py
+++ todo.py
@@ -4,8 +4,9 @@ class Todo():
 	"""
 	Contains a set of prioritized notes that get saved locally to an sqlite3 database.
 	"""
-	def __init__(self, filename=":memory:"):
-		self.database = sqlite3.connect(filename)
+	def __init__(self):
+		# Makes our database in memory, as opposed to in a file.
+		self.database = sqlite3.connect(":memory:")
 		self.cursor = self.database.cursor()
 
 		# if our table doesn't already exist, make it
@@ -20,18 +21,31 @@ class Todo():
 			CREATE TABLE notes(
 				id INTEGER PRIMARY KEY,
 				priority FLOAT NOT NULL,
-				dueDate date,
+				completedOn date,
 				text varchar(250) NOT NULL UNIQUE
 			)		
 		""")
 
+	def _flushExceptions(self):
+		"""
+		do i need this to get exceptions?  pyodbc needed something like this, but it seems like sqlite3 may not.  do more tests!
+		"""
+		#return
+		self.cursor.fetchall()
+
 # close database connection
 	def endSession(self):
+		"""
+		Closes the connection to the database, allowing memory to be freed and making this object unusable.
+		"""
 		#self.cursor.close()
 		self.database.close()
 
 # clear notes
 	def clearNotes(self):
+		"""
+		Removes all records from the database.
+		"""
 		# sqlite doesn't have truncate; this is the advised alternative
 		self.cursor.execute(r"DROP TABLE notes")
 		self._createTable()
@@ -39,33 +53,42 @@ class Todo():
 # see my existing notes
 	def getNotes(self):
 		"""
-		Returns a list of tuples containing [id, text, priority, dueDate]
+		Returns a list of tuples containing (text, priority, completedOn)
 		"""
-		self.cursor.execute(r"SELECT id, text, priority, dueDate FROM notes ORDER BY priority ASC, dueDate DESC, text ASC")
+		self.cursor.execute(r"SELECT text, priority, completedOn FROM notes ORDER BY priority ASC, completedOn DESC, text ASC")
 		noteList = []
 		for note in self.cursor:
 			noteList.append(note)
+		
+		self._flushExceptions()
 		return noteList
 
 # add new note
-	def addNote(self, text, priority, dueDate=None):
-		if priority < 0:
+	def addNote(self, text, priority, completedOn=None):
+		"""
+		Add a new note to the Todo.  Lower values for priority are considered more important.
+		"""
+		if priority < 1:
 			raise ValueError(r"Priority must have a non-negative value.")
 		
-		insertQuery = r"INSERT INTO notes(text, priority, dueDate) VALUES(?, ?, ?)"
-		values = [text, priority, dueDate]
+		insertQuery = r"INSERT INTO notes(text, priority, completedOn) VALUES(?, ?, ?)"
+		values = [text, priority, completedOn]
 		self.cursor.execute(insertQuery, values)
 		self.database.commit()
-		return self.cursor.fetchall()
+		
+		self._flushExceptions()
 
 
 # remove note
 	
 
 # update note
+	
+
+# save note
 
 
-
+# load note
 
 
 
@@ -73,74 +96,4 @@ class Todo():
 
 
 if __name__ == '__main__':
-	pass
-
-
-
-
-
-
-
-######################################################
-
-def testDatabaseFunctionality():
-	# connect to DB (makes it if it doesn't exist)
-	tempConnection = sqlite3.connect("temp.db")
-	
-	# execute a query that returns a list of all tables named BestDataEver
-	tempCursor = tempConnection.cursor()
-	output = tempCursor.execute("SELECT name FROM sqlite_schema WHERE type='table' AND name='BestDataEver'")
-
-	# Here's something a bit weird; the execute method returns an alias of the cursor that calls it.
-	if tempCursor is output:
-		print(r"Oh, it looks like cursor.execute() returns cursor...?")
-
-	#
-
-class easySQL:
-	"""
-	A wrapper for an sqlite3 connection/cursor that automates the extraction of results after queries.
-	"""
-	def __init__(self, databaseFilename):
-		self.connection = sqlite3.connect(databaseFilename)
-		self.cursor = self.connection.cursor()
-
-	def query(self, sql):
-		self.cursor.execute(sql)
-		return self.cursor.fetchall()
-
-	def commit(self):
-		return self.connection.commit()
-
-
-def fakeStuff():
-	database = easySQL("mystery.db")
-	
-	# query checks if a table exists
-	output = database.query("SELECT name FROM sqlite_schema WHERE type='table' AND name='notes'")
-	print(output)
-
-	if len(output) == 0:
-		database.query("""	
-			CREATE TABLE notes(
-				id INTEGER PRIMARY KEY,
-				importance FLOAT NOT NULL,
-				note varchar(250) NOT NULL UNIQUE
-			)		
-		""")
-
-		output = database.query("SELECT name FROM sqlite_schema WHERE type='table' AND name='notes'")
-		print(output)
-		
-		database.query("INSERT INTO notes(importance, note) VALUES(3.5, 'eat a sandwich')")
-		database.query("INSERT INTO notes(importance, note) VALUES(86, 'do a stretch')")
-		database.connection.commit()
-
-	output = database.query("SELECT * FROM notes")
-	print(output)
-
-
-
-
-
-	
+	myTodo = Todo()
